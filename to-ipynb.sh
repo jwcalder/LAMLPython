57ce40767b2b156ce32a9183e835cff3756dfbae
Jeff Calder <jcalder@umn.edu>
2025-01-09 15:28:59 -0600

    Updates

--- to-ipynb.sh
+++ to-ipynb.sh
@@ -9,9 +9,12 @@ pyfile=$1
 echo "Converting $pyfile to "$(dirname "$pyfile")/$(basename "$pyfile" .py).ipynb"..."
 
 url=https://colab.research.google.com/github/jwcalder/LAMLPython/blob/main/"$(dirname "$pyfile")/$(basename "$pyfile" .py).ipynb"
-qr_name="$(dirname "$pyfile")_$(basename "$pyfile" .py).png"
-echo Creating QR code for $url and storing in QRCodes/$qr_name
-qrencode -o QRCodes/$qr_name $url
+qr_name_eps="$(dirname "$pyfile")_$(basename "$pyfile" .py).eps"
+qr_name_pdf="$(dirname "$pyfile")_$(basename "$pyfile" .py).pdf"
+echo Creating QR code for $url and storing in QRCodes/$qr_name_pdf
+qrencode -m 4 -v 6 -o QRCodes/$qr_name_eps $url
+magick QRCodes/$qr_name_eps QRCodes/$qr_name_pdf
+rm QRCodes/$qr_name_eps
 
 sed 's/#pip install/pip install/' $pyfile | sed '/plt.ion/d' > temp.py
 ipynb-py-convert temp.py "$(dirname "$pyfile")/$(basename "$pyfile" .py).ipynb"
